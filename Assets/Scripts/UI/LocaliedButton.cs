080943106dc3204e970055fb429baf754aa79a37
Léa L <47750546+Lealaf@users.noreply.github.com>
2026-03-21 15:01:04 +0100

    🚧 Add button

---
+++ Assets/Scripts/UI/LocaliedButton.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+
+public class LocaliedButton: MonoBehaviour
+{
+    public Button button;
+    List<Locale> localizedLangues;
+    int selected;
+
+    IEnumerator Start()
+    {
+        // Wait for the localization system to initialize, loading Locales, preloading etc.
+        yield return LocalizationSettings.InitializationOperation;
+
+        localizedLangues = new List<Locale>();
+
+        // Generate list of available Locales
+        for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
+        {
+            var locale = LocalizationSettings.AvailableLocales.Locales[i];
+            if (LocalizationSettings.SelectedLocale == locale)
+                selected = i;
+            localizedLangues.Add(locale);
+        }
+    }
+
+    static void LocaleSelected(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+
+    public void LangueSwitch()
+    {
+        selected++;
+        if (selected>= LocalizationSettings.AvailableLocales.Locales.Count)
+        {
+            selected = 0;
+        }
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[selected];
+
+    }
+}

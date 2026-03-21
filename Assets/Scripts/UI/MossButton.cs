efe0b0544ce5a769911088fd7fe44c31dfdc25a8
Léa L <47750546+Lealaf@users.noreply.github.com>
2026-03-21 18:27:06 +0100

    💄over event button

--- Assets/Scripts/UI/MossButton.cs
+++ Assets/Scripts/UI/MossButton.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MossButton : MonoBehaviour
@@ -14,7 +15,18 @@ public class MossButton : MonoBehaviour
     {
         this.id = id;
         this.image.sprite = sprite;
-        //TODO over
-        this.button.onClick.AddListener(() => changeSelectedMoss(id));
+
+        //Hover event
+        EventTrigger trigger = button.GetComponentInParent<EventTrigger>();
+
+        if (trigger == null) trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+
+        entry.eventID = EventTriggerType.PointerEnter;
+
+        entry.callback.AddListener((eventData) => { changeSelectedMoss(id); });
+
+        trigger.triggers.Add(entry);
     }
 }

447a14a19cedb1110a8c3008f97bfc2d3ade5eb5
Léa L <47750546+Lealaf@users.noreply.github.com>
2026-03-21 16:19:48 +0100

    🚧 add MossManager

--- Assets/Scripts/UI/ExplainCanvas.cs
+++ Assets/Scripts/UI/ExplainCanvas.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ExplainCanvas : MonoBehaviour
 {
+
+    GameObject content;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,24 @@ public class ExplainCanvas : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SetState(int id)
+    {
+        if (id == -1)
+        {
+            Show(false);
+        }
+        else
+        {
+            Show(true);
+        }
+    }
+
+    private void Show( bool show)
+    {
+        //anim here todo
+        content.SetActive(show);
+
     }
 }

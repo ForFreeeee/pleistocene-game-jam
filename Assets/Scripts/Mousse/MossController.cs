e5ffc8c50ba0f971d309298e42664e859ca61457
ForFreeeee <ben.schramme@gmail.com>
2026-04-21 01:21:27 +0200

    Add check for moss lifespan

--- Assets/Scripts/Mousse/MossController.cs
+++ Assets/Scripts/Mousse/MossController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class MossController : MonoBehaviour
 {
     public MossStateMachine MossStateMachine;
-    public MossData MossData;
+   public MossCoverable moss_coverable;
+    public Vector3 hitPoint;
+    public Vector2 textureCoord;
+     public MossData MossData;
+    public GameObject directionalLight;
+    public MossManageur gameManageur;
 
     [SerializeField]
     private bool willDie;
@@ -29,9 +34,34 @@ public class MossController : MonoBehaviour
         MossStateMachine.PhysicsUpdate();
     }
     
-    public void AnticipateDeath()
-    {
-        willDie=true;
+public void ShouldMossLive(string hitTag)
+     {
+        willDie=false;
+        Ray ray = new Ray(transform.position, directionalLight.transform.position - transform.position);
+        RaycastHit hit;
+        bool hitSun = Physics.Raycast(ray, out hit);
+        //Debug.Log(hit.point);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 3.0f);
+        if ( hitSun && !MossData.LikesSun || !hitSun && MossData.LikesSun)
+        {
+            Debug.Log("Can't place moss here, No direct sunlight");
+            willDie=true;
+        }
+        if(MossData.LikesGround && hitTag!="Ground")
+        {
+            Debug.Log("Can't place moss here, Need ground");
+            willDie=true;
+        }
+        if(MossData.LikesWater && hitTag!="Water")
+        {
+            Debug.Log("Can't place moss here, Need water");
+            willDie=true;   
+        }
+        if(MossData.LikesRock && hitTag!="Rock")
+        {
+            Debug.Log("Can't place moss here, Need rock");
+            willDie=true;
+        }
     }
 
     public bool mustDie()
--- Assets/Scripts/Mousse/States/GrowthState.cs
+++ Assets/Scripts/Mousse/States/GrowthState.cs
@@ -30,6 +30,8 @@ namespace GameDev.Moss.States
             moss.transform.localScale=Vector3.Lerp(moss.transform.localScale, moss.MossData.finalGrowthSize, t);
             if (growthTimer <= 0)
             {
+                float surfaceArea = moss.moss_coverable.AddMoss(moss.hitPoint, moss.textureCoord);
+                moss.gameManageur.surfaceCovered += surfaceArea;
                 moss.MossStateMachine.TransitionTo(moss.MossStateMachine.matureState);
                 return;
             }
